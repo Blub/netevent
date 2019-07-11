392959e028c05e6d0334f67282a165cf46f7cacb
Wolfgang Bumiller <w.bumiller@errno.eu>
2019-07-11 18:37:35 +0200

    use spdx license identifier and bump copyright year
    Signed-off-by: Wolfgang Bumiller <w.bumiller@errno.eu>

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,9 @@
 /*
  * netevent - low-level event-device sharing
  *
- * Copyright (C) 2017,2018 Wolfgang Bumiller <wry.git@bumiller.com>
+ * Copyright (C) 2017-2019 Wolfgang Bumiller <wry.git@bumiller.com>
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * SPDX-License-Identifier: GPL-2.0-or-later
  */
 #include <getopt.h>
 #include <sys/types.h>
