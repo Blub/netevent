378e2696667517bcda261396f683e5a8a092a2cf
Wolfgang Bumiller <w.bumiller@errno.eu>
2019-07-11 18:23:59 +0200

    daemon: don't clear command client fds
    Socket::accept already passes SOCK_CLOEXEC to accept4()
    
    Signed-off-by: Wolfgang Bumiller <w.bumiller@errno.eu>

--- src/daemon.cpp
+++ src/daemon.cpp
@@ -129,7 +129,6 @@ static void parseClientCommand(int clientfd, const char *cmd, size_t length);
 static void
 daemon_preExec()
 {
-	gCommandClients.clear(); // closes fds
 	gInputs.clear(); // closes event devices
 	gOutputs.clear();
 	gFDCBs.clear();
